6502257718116a97111dc86a3368a93fdfa435cf
NizarDarwish <nizardarwish001@gmail.com>
2022-01-06 16:45:35 +0200

    bug fix

--- forecast.py
+++ forecast.py
@@ -22,10 +22,8 @@ if len(sys.argv) != 5:
 df=pd.read_csv(sys.argv[2], delimiter='\t', header=None)
 print('Number of rows and columns:', df.shape)
 
-training_set = df.iloc[:200].values[:1]
-test_set = df.iloc[200:].values[:1]
-
-print(training_set)
+training_set = df.iloc[:200, 1:].values
+test_set = df.iloc[200:, 1:].values
 
 # Feature Scaling
 sc = MinMaxScaler(feature_range = (0, 1))
@@ -34,10 +32,11 @@ training_set_scaled = sc.fit_transform(training_set)
 # Creating a data structure with 60 time-steps and 1 output
 X_train = []
 y_train = []
-for i in range(60, 800):
+for i in range(60, 200):
     X_train.append(training_set_scaled[i-60:i, 0])
     y_train.append(training_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
 
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-#(740, 60, 1)
+
+print(X_train)
